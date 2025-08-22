75b1fa2f556c875e37f6fba6c04d388603bff066
helouaziz <ouazizi2code@gmail.com>
2025-08-22 15:11:49 +0100

    add models extravting in views

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import  Question
 
 # Create your views here.
 
 
 def index(req):
-    return HttpResponse("hello from the polls app")
+    latest_Questions = Question.objects.all().order_by("-pub_date")[:5]
+    # lets clear the output
+    output = ", ".join([q.question_text for q in latest_Questions])
+    return HttpResponse(output)
+
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
